ac2351b09503da9429f9cb040f7df65d20cde6ee
Manish-webx <kushwahamanish956541@gmail.com>
2025-12-16 00:12:35 +0530

    deploy.php changes

--- deploy.php
+++ deploy.php
@@ -1,29 +1,25 @@
 <?php
 /**
  * GIT DEPLOYMENT SCRIPT
- *
  * Used for automatically deploying websites via GitHub Webhooks.
  */
 
-// The commands to run should ok?. 
-// 1. Go to the project folder (Step 2A repository path)
-// 2. Pull the latest changes
-// 3. Run the cPanel deployment (process .cpanel.yml tasks)
+// Commands to run
 $commands = array(
     'cd /home/laravelbp/public_html/',
-    '/usr/local/bin/git pull', 
-    '/usr/local/bin/php artisan migrate --force' // Optional: Run migrations automatically
+    '/usr/local/bin/git pull',
+    '/usr/local/bin/php artisan migrate --force'
 );
 
-// Run the commands
+// Run commands
 $output = '';
-foreach($commands as $command){
-    $tmp = shell_exec($command . " 2>&1"); // Capture error output too
-    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-    $output .= htmlentities(trim($tmp)) . "\n";
-}
+foreach ($commands as $command) {
+    $tmp = shell_exec($command . " 2>&1");
 
-// Display output (useful for debugging if you visit the URL manually)
+    $output .= "<span style=\"color:#6BE234;\">$</span> ";
+    $output .= "<span style=\"color:#729FCF;\">{$command}</span>\n";
+    $output .= htmlentities(trim($tmp ?? '')) . "\n\n";
+}
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
@@ -31,9 +27,7 @@ foreach($commands as $command){
     <meta charset="UTF-8">
     <title>Git Deployment Script</title>
 </head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
-<?php echo $output; ?>
-</pre>
+<body style="background-color:#000;color:#fff;font-weight:bold;padding:10px;">
+<pre><?php echo $output; ?></pre>
 </body>
 </html>
